4f6b469151b6bf8d79952db2b2196401edd12180
Patrick Leask <patrickaaleask@gmail.com>
2023-05-07 19:12:10 +0100

    Fix path for virtual env

--- train_subject_model_batch.sh
+++ train_subject_model_batch.sh
@@ -10,4 +10,4 @@ source /etc/profile
 module load cuda/11.7
 
 source /home2/wclv88/bounding-mi/bin/activate
-stdbuf -oL /home2/wclv88/bounding-mi/bounding-mi/bounding-mi/bin/python train_subject_models.py subject_models $1 5 $1 addition 10000
+stdbuf -oL /home2/wclv88/bounding-mi/bounding-mi/bin/python train_subject_models.py subject_models $1 5 $1 addition 10000
