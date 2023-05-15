d36b1f75fc8a8bed4fd9455279e76fea648487cc
Patrick Leask <patrickaaleask@gmail.com>
2023-05-15 14:04:06 +0100

    Use uuid instead of date for experiment name

--- train_subject_models_pipeline.sh
+++ train_subject_models_pipeline.sh
@@ -1,8 +1,8 @@
-job_name=$(date)
+job_name=$(uuidgen)
 dir=experiments/$job_name
 mkdir $dir
 mkdir $dir/outs
 mkdir $dir/subject_models
-echo "function: $f epochs: $epochs" >> $dir/metadata.txt
+echo "date: $(date) function: $f epochs: $epochs" >> $dir/metadata.txt
 
 sbatch --job-name=$job_name train_subject_models.sh
