8123b9f9acb158a46f88f0c26369f558a8f2bca1
Patrick Leask <patrickaaleask@gmail.com>
2023-05-15 14:12:10 +0100

    Nicer metadata formatting

--- train_subject_models_pipeline.sh
+++ train_subject_models_pipeline.sh
@@ -6,6 +6,8 @@ mkdir $dir/subject_models
 
 f='addition'
 epochs=10000
-echo "date: $(date) function: $f epochs: $epochs" >> $dir/metadata.txt
+echo "date: $(date)" >> $dir/metadata.txt
+echo "function: $f" >> $dir/metadata.txt
+echo "epochs: $epochs" >> $dir/metadata.txt
 
 sbatch --job-name=$job_name train_subject_models.sh $f $epochs
