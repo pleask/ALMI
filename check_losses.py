97ca1985f6daca32e9fe9ca5ed528685727ab2f6
Patrick Leask <patrickaaleask@gmail.com>
2023-05-16 20:20:18 +0100

    add parameters to check losses script

--- check_losses.py
+++ check_losses.py
@@ -2,19 +2,23 @@ import os
 import json
 import numpy as np
 import termplotlib as tpl
+import sys
 
-losses = []
-for file_name in os.listdir('subject_models'):
-    if file_name[-4:] != 'json':
-        continue
-    with open(f'subject_models/{file_name}', 'r') as f:
-        j = json.load(f)
-    loss = float(j['loss'])
-    if loss < 0.000001:
-        losses.append(float(j['loss']))
+if __name__ == '__main__':
+    cutoff = float(sys.argv[1])
+    model_dir = sys.argv[2]
+    losses = []
+    for file_name in os.listdir(f'{model_dir}'):
+        if file_name[-4:] != 'json':
+            continue
+        with open(f'{model_dir}/{file_name}', 'r') as f:
+            j = json.load(f)
+        loss = float(j['loss'])
+        if loss < cutoff:
+            losses.append(float(j['loss']))
 
-counts, bin_edges = np.histogram(losses)
+    counts, bin_edges = np.histogram(losses)
 
-fig = tpl.figure()
-fig.hist(counts, bin_edges, orientation="horizontal", force_ascii=False)
-fig.show()
+    fig = tpl.figure()
+    fig.hist(counts, bin_edges, orientation="horizontal", force_ascii=False)
+    fig.show()
