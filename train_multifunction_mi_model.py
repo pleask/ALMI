b80112e230da186ce88b2590a60e3d143d79be12
Patrick Leask <patrickaaleask@gmail.com>
2023-06-05 12:14:01 +0100

    add log lines

--- train_multifunction_mi_model.py
+++ train_multifunction_mi_model.py
@@ -113,7 +113,9 @@ def get_matching_subject_models_names(subject_model_dir, max_loss, weight_decay)
     for filename in all_subject_model_filenames:
         if not filename.endswith('.pickle'):
             continue
+        print('filename', filename)
         subject_model_name = filename.removesuffix('.pickle')
+        print('subject_model_name', subject_model_name)
         metadata = get_subject_model_metadata(subject_model_name)
         if metadata['loss'] > max_loss:
             pass
@@ -123,7 +125,7 @@ def get_matching_subject_models_names(subject_model_dir, max_loss, weight_decay)
     return matching_names
 
 
-@cache
+@cache()
 def get_subject_model_metadata(self, subject_model_name):
     with open(f'{self._subject_model_dir}/{subject_model_name}_metadata.json') as f:
         return json.load(f)
@@ -159,7 +161,7 @@ class MultifunctionSubjectModelDataset(Dataset):
         return x, y
 
     # This might run out of memory
-    @cache
+    @cache()
     def _get_subject_model(self, subject_model_name):
         net = get_subject_net()
         net.load_state_dict(torch.load(f"{self._subject_model_dir}/{subject_model_name}.pickle"))
