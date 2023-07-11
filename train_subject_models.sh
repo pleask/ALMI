9eb8b8e41033346125ea2c806522eef3a7f79dca
Patrick Leask <patrickaaleask@gmail.com>
2023-07-11 19:48:20 +0100

    changes to subject model slurm scripts

--- train_subject_models.sh
+++ train_subject_models.sh
@@ -3,14 +3,12 @@
 #SBATCH -c 1
 #SBATCH -p cpu
 #SBATCH --time=0-00:10:00
-#SBATCH -o subject_model_outs/%A_%a.out
+#SBATCH -o subject_models_2_outs/%A_%a.out
 
 epochs=5
 weight_decay=$1
-dir=subject_models_new
+dir=subject_models_2
 
 source /etc/profile
-module load cuda/11.7
-source /home2/wclv88/bounding-mi/bounding-mi/bin/activate
 echo $epochs $weight_decay
-stdbuf -oL /home2/wclv88/bounding-mi/bounding-mi/bin/python train_subject_models.py --path=$dir --index=$SLURM_ARRAY_TASK_ID --seed=0 --epochs=$epochs --weight_decay=$weight_decay
+stdbuf -oL /home2/wclv88/bounding-mi/bin/python train_subject_models.py --path=$dir --index=$SLURM_ARRAY_TASK_ID --seed=0 --epochs=$epochs --weight_decay=$weight_decay
