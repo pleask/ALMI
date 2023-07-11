a7930f08a9d8913d24444bffbe3edfea9147eb29
Patrick Leask <patrickaaleask@gmail.com>
2023-07-11 18:55:10 +0100

    Train on cpu with slurm

--- train_subject_models.sh
+++ train_subject_models.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -c 1
-#SBATCH --gres=gpu:1
-#SBATCH -p res-gpu-small
-#SBATCH --qos=short
+#SBATCH -p cpu
 #SBATCH --time=0-00:10:00
-#SBATCH --array=0-0
 #SBATCH -o subject_model_outs/%A_%a.out
 
-f=$1
-epochs=10000
-weight_decay=$2
-dir=subject_models
+epochs=5
+weight_decay=$1
+dir=subject_models_new
 
 source /etc/profile
 module load cuda/11.7
-i=$((5*$SLURM_ARRAY_TASK_ID+1))
 source /home2/wclv88/bounding-mi/bounding-mi/bin/activate
-echo $f $epochs $weight_decay
-stdbuf -oL /home2/wclv88/bounding-mi/bounding-mi/bin/python train_subject_models.py --path=$dir --count=5 --seed=$i --fn_name=$f --epochs=$epochs --weight_decay=$weight_decay
+echo $epochs $weight_decay
+stdbuf -oL /home2/wclv88/bounding-mi/bounding-mi/bin/python train_subject_models.py --path=$dir --index=$SLURM_ARRAY_TASK_ID --seed=0 --epochs=$epochs --weight_decay=$weight_decay
--- train_subject_models_pipeline.sh
+++ train_subject_models_pipeline.sh
@@ -1,11 +1,6 @@
-declare -a functions=("addition" "multiplication" "sigmoid" "exponent" "min")
 declare -a weight_decays=("0" "0.0001" "0.001" "0.01" "0.1" "1")
-for function in "${functions[@]}"
+for weight_decay in "${weight_decays[@]}"
 do
-    for weight_decay in "${weight_decays[@]}"
-    do
-        echo $function $weight_decay
-        sbatch --array=0-2000 train_subject_models.sh $function $weight_decay
-    done
+    echo $function $weight_decay
+    sbatch --array=0-2000 train_subject_models.sh $weight_decay
 done
-
