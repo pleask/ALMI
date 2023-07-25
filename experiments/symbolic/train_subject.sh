76d942be7823aeb378b64a54f9ef1b6d78972e3f
Patrick Leask <patrickaaleask@gmail.com>
2023-07-25 10:10:06 +0100

    use new output directory structure

--- experiments/symbolic/train_subject.sh
+++ experiments/symbolic/train_subject.sh
@@ -3,7 +3,7 @@
 #SBATCH -c 1
 #SBATCH -p cpu
 #SBATCH --time=0-06:00:00
-#SBATCH -o bounding_subject_models_out/%j.out
+#SBATCH -o bounding-mi-data/symbolic/outs
 #SBATCH --array 0-9999
 
 source /etc/profile
@@ -13,4 +13,4 @@ start_index=$((SLURM_ARRAY_TASK_ID*batch_size))
 end_index=$(((SLURM_ARRAY_TASK_ID+1)*batch_size))
 
 source /etc/profile
-stdbuf -oL /home2/wclv88/bounding-mi/bin/python bounding-mi-repo/experiments/symbolic/train_subject.py --path ~/bounding_subject_models --seed 0 --start_idx $start_index --end_idx $end_index
+stdbuf -oL /home2/wclv88/bounding-mi/bin/python bounding-mi-repo/experiments/symbolic/train_subject.py --path ~/bounding-mi-data/symbolic/subject-models --seed 0 --start_idx $start_index --end_idx $end_index
