b496aad992ad92aa086fac126c5b3a2e9dd0cb8e
Patrick Leask <patrickaaleask@gmail.com>
2023-10-02 12:11:24 +0100

    Fix import

--- experiments/integer/pretrain.py
+++ experiments/integer/pretrain.py
@@ -3,7 +3,7 @@ Pre-train subject models for the different optimisers. This allows great
 parallelism on Hamilton, rather than needing to train all the subject models as
 part of the pipeline.
 """
-from auto_mi.mi import pretrain_subject_models
+from auto_mi.rl import pretrain_subject_models
 from .pipeline import OPTIMISER_MODEL, SUBJECT_MODEL, SUBJECT_MODEL_PATH, TASK
 
 if __name__ == '__main__':
