4ab75b3a10f062d4e2f89f029a726b093d88e6ce
Patrick Leask <patrickaaleask@gmail.com>
2023-10-08 17:00:24 +0100

    Introduce array training method

--- experiments/integer/pipeline.py
+++ experiments/integer/pipeline.py
@@ -26,13 +26,11 @@ INTERPRETABILITY_MODEL_EPOCHS = 5
 TASK = IntegerGroupFunctionRecoveryTask(2**1 - 1, 2)
 SUBJECT_MODEL = IntegerGroupFunctionRecoveryModel
 HYPERPARAMETERS = {
-    'weight_decay': [0, ], #0.1],
-    'learning_rate': [0.1, ], #0.01],
     'weight_decay': [0, 0.001, 0.01, 0.1],
     'learning_rate': [0.1, 0.01, 0.001, 0.0001],
 }
 STATE_SPACE = [AdamTrainer(TASK, SUBJECT_MODEL_EPOCHS, SUBJECT_MODELS_BATCH_SIZE, weight_decay=wd, lr=lr, device=DEVICE) for wd in HYPERPARAMETERS['weight_decay'] for lr in HYPERPARAMETERS['learning_rate']]
-OPTIMISER_MODEL = QLearner(STATE_SPACE)
+OPTIMISER_MODEL = QLearner
 
 # Set up the interpretability model
 # TODO: Having to initialise a sample model is not lovely
@@ -41,6 +39,7 @@ sample_model = SUBJECT_MODEL(TASK)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run either pretraining or the full pipeline.')
     parser.add_argument("--pretrain", action="store_true", help="Pretrain models rather than running the full pipeline")
+    parser.add_argument("--array_number", type=int, default=-1, help="Test a single hyperparameter combination rather than running the full pipeline. This facilitates parallelisation.")
     parser.add_argument("--subject_model_path", type=str, help="Location of the subject models. If writing to this location (ie. when pretraining or running the RL pipeline with training new subject models) it must be a directory. If only reading from this location (ie. when running the pipeline without training new subject models), can be a tar archive.")
     args = parser.parse_args()
 
@@ -55,13 +54,21 @@ if __name__ == '__main__':
         subject_model_parameter_count = sum(p.numel() for p in sample_model.parameters())
         print(f'Subject model parameter count: {subject_model_parameter_count}')
 
-        interpretability_models = [Transformer(subject_model_parameter_count, TASK.mi_output_shape).to(DEVICE) for _ in STATE_SPACE]
+        state_space = STATE_SPACE
+        name = 'Full pipeline run'
+        if args.array_number >= 0:
+            state_space = [STATE_SPACE[args.array_number]]
+            name = f'Array run {args.array_number}: {state_space[0].get_metadata()}'
+
+        optimiser_model = OPTIMISER_MODEL(state_space)
+
+        interpretability_models = [Transformer(subject_model_parameter_count, TASK.mi_output_shape).to(DEVICE) for _ in state_space]
 
         # TODO: Use an evaluator structure instead of function on task
         os.environ["WAND_API_KEY"] = "dd685d7aa9b38a2289e5784a961b81e22fc4c735"
-        wandb.init(project='bounding-mi', entity='patrickaaleask', reinit=True)
+        wandb.init(project='bounding-mi', entity='patrickaaleask', reinit=True, name=name)
 
-        train_optimiser_model(OPTIMISER_MODEL, interpretability_models, model_writer, SUBJECT_MODEL, TASK, EPISODES, STEPS, subject_models_per_step=SUBJECT_MODELS_PER_STEP)
+        train_optimiser_model(optimiser_model, interpretability_models, model_writer, SUBJECT_MODEL, TASK, EPISODES, STEPS, subject_models_per_step=SUBJECT_MODELS_PER_STEP)
 
         # Return the best hyperparameters
         print(f"Optimal weight decay is {OPTIMISER_MODEL.get_optimal().weight_decay}, optimal lr is {OPTIMISER_MODEL.get_optimal().lr}")
