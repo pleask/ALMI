154621200bd8482db153975c8871b9cadf2f5381
Patrick Leask <patrickaaleask@gmail.com>
2023-10-10 16:58:15 +0100

    hamiltion dir move

--- experiments/integer/pretrain.sh
+++ experiments/integer/pretrain.sh
@@ -3,7 +3,7 @@
 #SBATCH -c 1
 #SBATCH -p shared
 #SBATCH --time=0-06:00:00
-#SBATCH -o /nobackup/wclv88/bounding-mi-data/integer/outs/slurm-%A_%a.out
+#SBATCH -o /nobackup/wclv88/integer/outs/slurm-%A_%a.out
 #SBATCH --array 0-9999
 
 module load python/3.10.8
@@ -11,5 +11,5 @@ module load $PYTHON_BUILD_MODULES
 # pip install /home/wclv88/bounding-mi-repo/auto_mi
 
 for index in $(seq 0 19); do
-	stdbuf -oL python3 bounding-mi-repo/experiments/integer/pipeline.py --pretrain --subject_model_path /nobackup/wclv88/bounding-mi-data/integer/subject-models
+	stdbuf -oL python3 bounding-mi-repo/experiments/integer/pipeline.py --pretrain --subject_model_path /nobackup/wclv88/integer/subject-models
 done
