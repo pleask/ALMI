c51cd907a861383b49d549e9cc9d6a102930c4d5
Patrick Leask <patrickaaleask@gmail.com>
2023-10-20 15:27:02 +0100

    Fix slurm script

--- experiments/mnist_permuted/pretrain.sh
+++ experiments/mnist_permuted/pretrain.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -c 4
-#SBATCH -p shared
-#SBATCH --time=0-06:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -p res-gpu-small
+#SBATCH --qos=short
+#SBATCH --time=2-00:00:00
 #SBATCH --output=mnist/outs/%A_%a.out
 #SBATCH --array 0-9999
 #SBATCH --exclude gpu4,gpu5,gpu6
@@ -13,5 +15,6 @@ module load cuda/11.7
 source /etc/profile
 
 for index in $(seq 0 4); do
-	stdbuf -oL python3 bounding-mi-repo/experiments/mnist_perturbed/benchmark.py --seed 
+    SEED=$(( $SLURM_ARRAY_TASK_ID * 5 + $index ))
+	stdbuf -oL bounding-mi/bin/python bounding-mi-repo/experiments/mnist_permuted/benchmark.py --seed $SEED
 done
