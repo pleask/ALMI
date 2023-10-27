91aea6919e6feb3f4f399c8d2a4abc9cf1eb63c7
Patrick Leask <patrickaaleask@gmail.com>
2023-10-27 12:16:44 +0100

    Add L1 regularisation

--- auto_mi/auto_mi/trainers.py
+++ auto_mi/auto_mi/trainers.py
@@ -8,57 +8,27 @@ from torch.utils.data import DataLoader
 from torch.nn.utils.prune import L1Unstructured
 
 from .base import MetadataBase
-from .tasks import to_int
 
 
 class BaseTrainer(MetadataBase, ABC):
+    """
+    Implements the training loop based on passed optimisers.
+    """
+    def __init__(self, task, epochs, batch_size):
+        self.task = task
+        self.epochs = epochs
+        self.batch_size = batch_size
+
     @abstractmethod
-    def train(self, model, example):
+    def train_parallel(self, models, examples, validation_examples):
         """
         Trains a new model on the given example.
         """
         pass
 
-
-class AdamTrainer(BaseTrainer):
-    def __init__(self, task, epochs, batch_size, lr=0.01, weight_decay=0., prune_amount=0., device=torch.device('cpu')):
-        self.task = task
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.device = device
-        self.weight_decay = weight_decay
-        self.prune_amount = prune_amount
-        self.lr = lr
-
-    def train(self, net, example, validation_example):
-        optimizer = optim.Adam(net.parameters(), lr=self.lr, weight_decay=self.weight_decay)
-        training_data = DataLoader(example, batch_size=self.batch_size)
-
-        for epoch in range(self.epochs):
-            for inputs, labels in training_data:
-                    optimizer.zero_grad()
-                    inputs, labels = inputs.to(self.device), labels.to(self.device)
-                    output = net(inputs)
-                    loss = self.task.criterion(output, labels)
-                    loss.backward()
-                    optimizer.step()
-            validation_loss = self.evaluate(net, validation_example)
-
-        validation_loss = self.evaluate(net, validation_example, final=True)
-
-        if self.prune_amount > 0.:
-            pruner = L1Unstructured(self.prune_amount)
-            for _, param in net.named_parameters():
-                param.data = pruner.prune(param.data)
-
-        return validation_loss
-
-    def train_parallel(self, nets, examples, validation_examples):
-        optimizers = [optim.Adam(net.parameters(), lr=self.lr, weight_decay=self.weight_decay) for net in nets]
-        training_dataloaders = [DataLoader(example, batch_size=self.batch_size, shuffle=True) for example in examples]
-
-        for epoch in tqdm(range(self.epochs), desc='Subject model epochs'):
-            for net, optimizer, training_data in zip(nets, optimizers, training_dataloaders):
+    def _train_inner(self, models, examples, validation_examples, optimizers, training_dataloaders, l1_penalty_weight=0.):
+        for _ in tqdm(range(self.epochs), desc='Subject model epochs'):
+            for net, optimizer, training_data in zip(models, optimizers, training_dataloaders):
                 net = net.to(self.device)
                 net.train()
                 for inputs, labels in training_data:
@@ -66,22 +36,15 @@ class AdamTrainer(BaseTrainer):
                     inputs, labels = inputs.to(self.device), labels.to(self.device)
                     output = net(inputs)
                     loss = self.task.criterion(output, labels)
+                    if l1_penalty_weight:
+                        loss += l1_penalty_weight * sum([torch.abs(param).sum() for param in net.parameters()])
                     loss.backward()
                     optimizer.step()
                 net.eval()
 
-        validation_losses = [self.evaluate(net, validation_example, final=True) for net, validation_example in zip(nets, validation_examples)]
+        return [self.evaluate(net, validation_example) for net, validation_example in zip(models, validation_examples)]
 
-        if self.prune_amount > 0.:
-            pruner = L1Unstructured(self.prune_amount)
-            for net in nets:
-                for _, param in net.named_parameters():
-                    param.data = pruner.prune(param.data)
-
-        return validation_losses
-
-    
-    def evaluate(self, net, example, final=False):
+    def evaluate(self, net, example):
         data = DataLoader(example, batch_size=self.batch_size)
         inputs, labels = next(iter(data))
         inputs = inputs.to(self.device)
@@ -91,9 +54,26 @@ class AdamTrainer(BaseTrainer):
             outputs = net(inputs)
         return self.task.criterion(outputs, labels).detach().cpu().item()
 
+
+class AdamTrainer(BaseTrainer):
+    def __init__(self, task, epochs, batch_size, lr=0.01, weight_decay=0., l1_penalty_weight=0., device=torch.device('cpu')):
+        super().__init__(task, epochs, batch_size)
+        self.device = device
+        self.weight_decay = weight_decay
+        self.lr = lr
+        self.l1_penalty_weight = l1_penalty_weight
+
+    def train_parallel(self, nets, examples, validation_examples):
+        optimizers = [optim.Adam(net.parameters(), lr=self.lr, weight_decay=self.weight_decay) for net in nets]
+        training_dataloaders = [DataLoader(example, batch_size=self.batch_size, shuffle=True) for example in examples]
+
+        validation_losses = self._train_inner(nets, examples, validation_examples, optimizers, training_dataloaders)
+
+        return validation_losses
+
     def get_metadata(self):
         return super().get_metadata() | {
             'weight_decay': self.weight_decay,
-            'prune_amount': self.prune_amount,
             'lr': self.lr,
+            'l1_penalty_weight': self.l1_penalty_weight,
         }
--- experiments/mnist_permuted/benchmark.py
+++ experiments/mnist_permuted/benchmark.py
@@ -2,7 +2,6 @@ import argparse
 from itertools import permutations
 import random
 
-import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
@@ -14,7 +13,7 @@ from auto_mi.base import MetadataBase
 from auto_mi.rl import pretrain_subject_models
 from auto_mi.tasks import Task, Example, TRAIN, VAL
 from auto_mi.trainers import AdamTrainer
-from auto_mi.utils import TarModelWriter
+from auto_mi.utils import DirModelWriter, TarModelWriter
 
 
 TRAIN_RATIO = 0.7
@@ -84,26 +83,33 @@ class PermutedMNISTExample(Example):
 class MNIST_CNN(nn.Module, MetadataBase):
     def __init__(self, *_):
         super(MNIST_CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, padding=2)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, padding=2)
-        self.fc1 = nn.Linear(64*7*7, 64)
-        self.fc2 = nn.Linear(64, 10)
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(4, 4, kernel_size=5, padding=2)
+        self.fc1 = nn.Linear(4*7*7, 8)
+        self.fc2 = nn.Linear(8, 8)
+        self.fc3 = nn.Linear(8, 8)
+        self.fc4 = nn.Linear(8, 8)
+        self.fc5 = nn.Linear(8, 10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2)
-        x = x.view(-1, 64*7*7)
+        x = x.view(-1, 4*7*7)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x)
         return F.log_softmax(x, dim=1)
 
 
-def evaluate(task, subject_model_io):
+def evaluate(subject_model_io):
     metadata = subject_model_io.get_metadata()
     for model_idx in range(len(metadata)):
         print(f'Model {model_idx}')
+        task = PermutedMNISTTask(seed=metadata[model_idx]['task']['seed'])
         example = task.get_dataset(metadata[model_idx]['index'], type=VAL)
         model_id = metadata[model_idx]['id']
         permutation_map = metadata[model_idx]['example']['permutation_map']
@@ -112,29 +118,34 @@ def evaluate(task, subject_model_io):
         
         for i in range(10):
             image, label = example[-i]
-            image_np = image.squeeze().numpy()
             prediction = model(image.unsqueeze(0))
             print(label, torch.argmax(prediction, -1).item())
-            plt.imshow(image_np, cmap='gray')
-            plt.title(f"Label: {label}, Prediction: {torch.argmax(prediction, -1).item()}")
-            plt.axis('off')
-            plt.show()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run either pretraining or the full pipeline.')
     parser.add_argument("--evaluate_subject_model", action='store_true', help="Evaluate a subject model.")
     parser.add_argument("--seed", type=float, help="Random seed.", default=0.)
+    parser.add_argument("--device", type=str, default="cuda", help="Device to train or evaluate models on")
     args = parser.parse_args()
 
-    task = PermutedMNISTTask(args.seed)
-    subject_model_io = TarModelWriter('mnist/subject-models')
+    subject_model_io = TarModelWriter('subject-models')
 
     if args.evaluate_subject_model:
-        evaluate(task, subject_model_io)
+        evaluate(subject_model_io)
     else:
-        weight_decays = [0., 0.01, 0.001, 0.0001]
-        state_space = [AdamTrainer(task, 100, 10000, weight_decay=wd, lr=0.001, device='cuda') for wd in weight_decays]
+        task = PermutedMNISTTask(args.seed)
+
+        base_net = MNIST_CNN()
+        model_path = f'experiments/mnist_permuted/base_net.pickle'
+        load_args = (model_path,) if args.device == 'cuda' else (model_path, {'map_location': torch.device('cpu')})
+        try:
+            base_net.load_state_dict(torch.load(*load_args))
+        except FileNotFoundError:
+            pass
+
+        l1_penalty_weights = [0.]
+        state_space = [AdamTrainer(task, 30, 10000, lr=0.01, l1_penalty_weight=l1pw, device=args.device) for l1pw in l1_penalty_weights]
         subject_model = MNIST_CNN
 
         sample_model = subject_model(task)
@@ -145,4 +156,4 @@ if __name__ == '__main__':
 
         print('Pretraining subject models')
         trainer = random.choice(state_space)
-        pretrain_subject_models(trainer, subject_model_io, subject_model, task, batch_size=1)
+        pretrain_subject_models(trainer, subject_model_io, subject_model, task, batch_size=5)
