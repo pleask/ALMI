0d26171321b154e8f5801ed8853bd2ce2101b0bf
Patrick Leask <patrickaaleask@gmail.com>
2023-10-27 12:44:27 +0100

    remove source /etc/profile

--- experiments/integer/pretrain.sh
+++ experiments/integer/pretrain.sh
@@ -6,13 +6,9 @@
 #SBATCH -o /nobackup/wclv88/bounding-mi-data/integer/outs/slurm-%A_%a.out
 #SBATCH --array 0-9999
 
-source /etc/profile
-
 module load python/3.10.8
 module load $PYTHON_BUILD_MODULES
 # pip install /home/wclv88/bounding-mi-repo/auto_mi
-
-source /etc/profile
 
 for index in $(seq 0 19); do
 	stdbuf -oL python3 bounding-mi-repo/experiments/integer/pipeline.py --pretrain --subject_model_path /nobackup/wclv88/bounding-mi-data/integer/subject-models
