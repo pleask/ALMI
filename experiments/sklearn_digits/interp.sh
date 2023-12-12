b1c019f84fb3a2233a6e78c11fce6500b3cea0f3
Patrick Leask <patrickaaleask@gmail.com>
2023-12-12 15:23:04 +0000

    fix home dir name

--- experiments/sklearn_digits/interp.sh
+++ experiments/sklearn_digits/interp.sh
@@ -10,4 +10,4 @@
 source /etc/profile
 module load cuda/11.7
 
-WAND_API_KEY=dd685d7aa9b38a2289e5784a961b81e22fc4c735  stdbuf -oL /home2/wclv88/bounding-mi/bin/python /home2/wclv88/bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed 0 --device cuda --subject_model_path sklearn_digits/subject_models --interpretability_model_path=sklearn_digits/interpretability_models
+WAND_API_KEY=dd685d7aa9b38a2289e5784a961b81e22fc4c735  stdbuf -oL /home3/wclv88/bounding-mi/bin/python /home3/wclv88/bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed 0 --device cuda --subject_model_path sklearn_digits/subject_models --interpretability_model_path=sklearn_digits/interpretability_models
