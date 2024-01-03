5b62b170d4bca831c109a998c5bbb81db0d4a0be
Patrick Leask <patrickaaleask@gmail.com>
2024-01-03 10:11:36 +0000

    Delete unused code from wine experiment

--- experiments/wine/benchmark.py
+++ experiments/wine/benchmark.py
@@ -104,52 +104,6 @@ class WineClassifier(nn.Module, MetadataBase):
         return self.softmax(x)
 
 
-class WineInterpretabilityModel(nn.Module, MetadataBase):
-    def __init__(self, in_size, out_shape, layer_scale=5):
-        super().__init__()
-        self.out_shape = out_shape
-        out_size = torch.zeros(out_shape).view(-1).shape[0]
-        self.fc1 = nn.Linear(in_size, int(64*layer_scale))
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(int(64*layer_scale), int(32*layer_scale))
-        self.relu2 = nn.ReLU()
-        self.fc3 = nn.Linear(int(32*layer_scale), out_size)
-        self.softmax = nn.Softmax(dim=-1)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu1(x)
-        x = self.fc2(x)
-        x = self.relu2(x)
-        x = self.fc3(x)
-        x = x.view(-1, *self.out_shape)
-        return self.softmax(x)
-
-
-def evaluate(subject_model_io):
-    metadata = subject_model_io.get_metadata()
-    accuracies = []
-    for model_idx in range(len(metadata)):
-        print(f'Model {model_idx}')
-        task = PermutedWineTask(seed=metadata[model_idx]['task']['seed'])
-        example = task.get_dataset(metadata[model_idx]['index'], type=VAL)
-        model_id = metadata[model_idx]['id']
-        permutation_map = metadata[model_idx]['example']['permutation_map']
-        print(f"Permutation map: {permutation_map}")
-        model = subject_model_io.get_model(WineClassifier(), model_id)
-        
-        correct = []
-        for _ in range(100):
-            i = random.randint(0, len(example)-1)
-            input, label = example[i]
-            prediction = model(torch.Tensor(input).unsqueeze(0))
-            # print(label, torch.argmax(prediction, -1).item())
-            correct.append((torch.argmax(prediction, -1) == label)[0].item())
-        accuracy = sum(correct) /  100
-        accuracies.append(accuracy)
-    print(sum(accuracies)/ len(accuracies))
-
-
 if __name__ == '__main__':
     train_cli(
         DirModelWriter,
