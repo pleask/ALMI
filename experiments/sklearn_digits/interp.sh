1246d7d19d9f337000b40f8ef1adda649b33b15c
Patrick Leask <patrickaaleask@gmail.com>
2024-01-08 09:32:47 +0000

    Interp model digit array

--- experiments/sklearn_digits/interp.sh
+++ experiments/sklearn_digits/interp.sh
@@ -3,11 +3,14 @@
 #SBATCH -c 4
 #SBATCH -p res-gpu-small
 #SBATCH --qos=short
-#SBATCH --time=2-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --output=sklearn_digits/outs/slurm-%j.out
-#SBATCH --gres=gpu:ampere:1
+#SBATCH --array=0-5
+#SBATCH --gres=gpu
 
 source /etc/profile
 module load cuda/11.7
 
-WAND_API_KEY=dd685d7aa9b38a2289e5784a961b81e22fc4c735  stdbuf -oL /home3/wclv88/bounding-mi/bin/python /home3/wclv88/bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed 0 --device cuda --subject_model_path sklearn_digits/subject_models --interpretability_model_path=sklearn_digits/interpretability_models --interpretability_batch_size 128 --interpretability_gradient_accumulation 8
+NUM_DIGITS=$((5 + SLURM_ARRAY_TASK_ID))
+
+NUM_DIGITS=$NUM_DIGITS WAND_API_KEY=dd685d7aa9b38a2289e5784a961b81e22fc4c735  stdbuf -oL /home3/wclv88/bounding-mi/bin/python /home3/wclv88/bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed 0 --device cuda --subject_model_path sklearn_digits/${NUM_DIGITS}/subject_models --interpretability_model_path=sklearn_digits/interpretability_models --interpretability_batch_size 8 
