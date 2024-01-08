f21874862d2da72a70241604ff3e2268dac6ad63
Patrick Leask <patrickaaleask@gmail.com>
2024-01-08 09:08:43 +0000

    Train 10k models for unfrozen

--- experiments/sklearn_digits/pretrain.sh
+++ experiments/sklearn_digits/pretrain.sh
@@ -5,21 +5,6 @@
 #SBATCH --qos=short
 #SBATCH --time=2-00:00:00
 #SBATCH --output=sklearn_digits/outs/slurm-%j.out
-#SBATCH --array 0-65
-
-source /etc/profile
-module load cuda/11.7
-
-SEED=$SLURM_ARRAY_TASK_ID
-stdbuf -oL python3 bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed $SEED --device cpu --train_subject_models --subject_model_path  --batch_size 100
-
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH -c 4
-#SBATCH -p res-gpu-small
-#SBATCH --qos=short
-#SBATCH --time=2-00:00:00
-#SBATCH --output=sklearn_digits/outs/slurm-%j.out
 #SBATCH --array=0-65
 
 source /etc/profile
@@ -29,4 +14,10 @@ module load cuda/11.7
 FROZEN=$((SLURM_ARRAY_TASK_ID % 11))
 NUM_DIGITS=$((5 + SLURM_ARRAY_TASK_ID / 11))
 
-stdbuf -oL FROZEN=$FROZEN NUM_DIGITS=$NUM_DIGITS WANDB_DISABLED=true python3 bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed 0 --device cuda --train_subject_models --subject_model_count 1000 --subject_model_path /nobackup/wclv88/sklearn_digits/${NUM_DIGITS}/subject-models 
+if [ $FROZEN -eq 10 ]; then
+    SUBJECT_MODEL_COUNT=10000
+else
+    SUBJECT_MODEL_COUNT=1000
+fi
+
+stdbuf -oL FROZEN=$FROZEN NUM_DIGITS=$NUM_DIGITS WANDB_DISABLED=true python3 bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed 0 --device cuda --train_subject_models --subject_model_count $SUBJECT_MODEL_COUNT --subject_model_path /nobackup/wclv88/sklearn_digits/${NUM_DIGITS}/subject-models 
