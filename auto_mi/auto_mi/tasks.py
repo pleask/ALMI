44812248732d4846a424bc72567d6b6d061c6d53
Patrick Leask <patrickaaleask@gmail.com>
2024-01-16 15:16:22 +0000

    Refactor train-test split and limit training examples

--- auto_mi/auto_mi/tasks.py
+++ auto_mi/auto_mi/tasks.py
@@ -116,12 +116,13 @@ class SimpleExample(Example, ABC):
             X = X[y < num_classes]
             y = y[y < num_classes]
 
-        # Only use the first num_examples to train
-        X = X[:num_examples]
-
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.2, random_state=42
         )
+
+        # Only use the first num_examples to train
+        X_train = X_train[:num_examples]
+        y_train = y_train[:num_examples]
 
         if type == TRAIN:
             self.X = X_train
