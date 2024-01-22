ce24e1092ad0a3972339b631ab6e07045fde03dd
Patrick Leask <patrickaaleask@gmail.com>
2024-01-22 17:47:09 +0000

    Remove subject model example count from model variant script

--- experiments/sklearn_digits/model_variants/pretrain.sh
+++ experiments/sklearn_digits/model_variants/pretrain.sh
@@ -11,4 +11,4 @@
 source /etc/profile
 module load cuda/11.7
 
-WANDB_DISABLED=true stdbuf -oL /home3/wclv88/bounding-mi/bin/python bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed 0 --device cuda --train_subject_models --subject_model_count 1000 --subject_model_path sklearn_digits/variant_data/subject-models --subject_model_num_classes 10 --subject_model_example_count $EXAMPLE_COUNT --subject_model_variant $((SLURM_ARRAY_TASK_ID))
+WANDB_DISABLED=true stdbuf -oL /home3/wclv88/bounding-mi/bin/python bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed 0 --device cuda --train_subject_models --subject_model_count 1000 --subject_model_path sklearn_digits/variant_data/subject-models --subject_model_num_classes 10 --subject_model_variant $((SLURM_ARRAY_TASK_ID))
