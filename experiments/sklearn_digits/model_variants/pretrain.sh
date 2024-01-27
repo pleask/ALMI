e08e9563acc1ef8ec492612fcca48a08a7bd3c25
Patrick Leask <patrickaaleask@gmail.com>
2024-01-27 11:04:44 +0000

    Only produce two variants

--- experiments/sklearn_digits/model_variants/pretrain.sh
+++ experiments/sklearn_digits/model_variants/pretrain.sh
@@ -5,10 +5,19 @@
 #SBATCH --qos=short
 #SBATCH --time=1-00:00:00
 #SBATCH --output=sklearn_digits/variant_data/outs/slurm-%j.out
-#SBATCH --array=0-99
+#SBATCH --array=0-19
 #SBATCH --gres=gpu
 
 source /etc/profile
 module load cuda/11.7
 
-WANDB_DISABLED=true stdbuf -oL /home3/wclv88/bounding-mi/bin/python bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed 0 --device cuda --train_subject_models --subject_model_count 1000 --subject_model_path sklearn_digits/variant_data/subject-models --subject_model_num_classes 10 --subject_model_variant $((SLURM_ARRAY_TASK_ID))
+WANDB_DISABLED=true \
+stdbuf -oL /home3/wclv88/bounding-mi/bin/python \
+bounding-mi-repo/experiments/sklearn_digits/benchmark.py \
+--seed 0 \
+--device cuda \
+--train_subject_models \
+--subject_model_count 1000 \
+--subject_model_path sklearn_digits/variant_data/subject-models \
+--subject_model_num_classes 10 \
+--subject_model_variant $((SLURM_ARRAY_TASK_ID % 2))
