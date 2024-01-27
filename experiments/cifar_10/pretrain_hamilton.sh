d30c4bfe8f2819f35bf0e289429b612c45228fa6
Patrick Leask <patrickaaleask@gmail.com>
2024-01-27 11:45:49 +0000

    Fix benchmark file path

--- experiments/cifar_10/pretrain_hamilton.sh
+++ experiments/cifar_10/pretrain_hamilton.sh
@@ -12,7 +12,7 @@ module load $PYTHON_BUILD_MODULES
 
 SEED=$SLURM_ARRAY_TASK_ID
 stdbuf -oL python3 \
-    bounding-mi-repo/experiments/mnist_permuted/benchmark.py \
+    bounding-mi-repo/experiments/cifar_10/benchmark.py \
     --seed $SEED \
     --device cpu \
     --subject_model_path /nobackup/wclv88/cifar_10/subject-models \
