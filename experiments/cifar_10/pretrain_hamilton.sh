49e327474b11288f7368cc73f782605cde700ac8
Patrick Leask <patrickaaleask@gmail.com>
2024-01-27 11:37:10 +0000

    add hamilton pretrain script

---
+++ experiments/cifar_10/pretrain_hamilton.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --mem=1G
+#SBATCH -p shared
+#SBATCH --time=2-00:00:00
+#SBATCH -o /nobackup/wclv88/cifar_10/outs/slurm-%A_%a.out
+#SBATCH --array 0-1999
+
+module load python/3.10.8
+module load $PYTHON_BUILD_MODULES
+
+SEED=$SLURM_ARRAY_TASK_ID
+stdbuf -oL python3 \
+    bounding-mi-repo/experiments/mnist_permuted/benchmark.py \
+    --seed $SEED \
+    --device cpu \
+    --subject_model_path /nobackup/wclv88/cifar_10/subject-models \
+    --subject_model_count 5 \
+    --subject_model_num_classes 10
