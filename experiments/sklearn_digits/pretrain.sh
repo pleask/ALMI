0c90889ca266284c55b3024c4ba7be089672194b
Patrick Leask <patrickaaleask@gmail.com>
2024-01-30 13:03:32 +0000

    update skelarn digit pretraining script

--- experiments/sklearn_digits/pretrain.sh
+++ experiments/sklearn_digits/pretrain.sh
@@ -5,12 +5,19 @@
 #SBATCH --qos=short
 #SBATCH --time=1-00:00:00
 #SBATCH --output=sklearn_digits/outs/slurm-%j.out
-#SBATCH --array=0-65
+#SBATCH --array=0-19
 #SBATCH --gres=gpu
 
 source /etc/profile
 module load cuda/11.7
 
-NUM_DIGITS=$((5 + SLURM_ARRAY_TASK_ID / 11))
-
-NUM_DIGITS=$NUM_DIGITS WANDB_DISABLED=true stdbuf -oL /home3/wclv88/bounding-mi/bin/python bounding-mi-repo/experiments/sklearn_digits/benchmark.py --seed 0 --device cuda --train_subject_models --subject_model_count 10000 --subject_model_path sklearn_digits/${NUM_DIGITS}/subject-models 
+WANDB_DISABLED=true \
+stdbuf -oL \
+/home3/wclv88/bounding-mi/bin/python \
+bounding-mi-repo/experiments/sklearn_digits/benchmark.py \
+--seed 0 \
+--device cuda \
+--train_subject_models \
+--subject_model_count 1000 \
+--subject_model_path sklearn_digits/standard/subject-models \
+--subject_model_num_classes 10
