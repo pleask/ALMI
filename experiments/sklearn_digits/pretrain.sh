eeb4a599e27b4eae3805e2b332ce0e812e12bb00
Patrick Leask <patrickaaleask@gmail.com>
2024-01-31 13:53:21 +0000

    Switch to hamilton for sklearn pretraining

--- experiments/sklearn_digits/pretrain.sh
+++ experiments/sklearn_digits/pretrain.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -c 4
-#SBATCH -p res-gpu-small
-#SBATCH --qos=short
-#SBATCH --time=1-00:00:00
-#SBATCH --output=sklearn_digits/outs/slurm-%j.out
-#SBATCH --array=0-19
-#SBATCH --gres=gpu
+#SBATCH -c 1
+#SBATCH --mem=4G
+#SBATCH -p shared
+#SBATCH --time=2-00:00:00
+#SBATCH --output=/nobackup/wclv88/sklearn_digits/outs/slurm-%A_%a.out
+#SBATCH --array=0-1999
 
-source /etc/profile
-module load cuda/11.7
+module load python/3.10.8
+module load $PYTHON_BUILD_MODULES
 
-COUNT=1000
+COUNT=10
 
 WANDB_DISABLED=true \
 stdbuf -oL \
 /home3/wclv88/bounding-mi/bin/python \
 bounding-mi-repo/experiments/sklearn_digits/benchmark.py \
 --seed 0 \
---device cuda \
+--device cpu \
 --train_subject_models \
 --subject_model_count $COUNT \
 --subject_model_path sklearn_digits/standard/subject-models \
