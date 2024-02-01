9726e6faffcd6693935485a0d5ef5b83c12abdbf
Patrick Leask <patrickaaleask@gmail.com>
2024-02-01 12:49:42 +0000

    Expand variants to full 100 dataset

--- experiments/sklearn_digits/model_variants/pretrain.sh
+++ experiments/sklearn_digits/model_variants/pretrain.sh
@@ -5,12 +5,13 @@
 #SBATCH -p shared
 #SBATCH --time=2-00:00:00
 #SBATCH --output=/nobackup/wclv88/sklearn_digits/outs/slurm-%A_%a.out
-#SBATCH --array=0-199
+#SBATCH --array=0-9999
 
 module load python/3.10.8
 module load $PYTHON_BUILD_MODULES
 
 COUNT=10
+VARIANT_COUNT=100
 
 WANDB_DISABLED=true \
 stdbuf -oL \
@@ -22,5 +23,5 @@ bounding-mi-repo/experiments/sklearn_digits/benchmark.py \
 --subject_model_count 10 \
 --subject_model_path /nobackup/wclv88/sklearn_digits/variant_data/subject-models \
 --subject_model_num_classes 10 \
---subject_model_variant $((SLURM_ARRAY_TASK_ID % 2)) \
---example_start_index $((SLURM_ARRAY_TASK_ID * COUNT))
+--subject_model_variant $((SLURM_ARRAY_TASK_ID % VARIANT_COUNT)) \
+--example_start_index $((SLURM_ARRAY_TASK_ID / VARIANT_COUNT * COUNT))
