a6f68627644ca818de2f35644b3830fa87e4f6b6
Patrick Leask <patrickaaleask@gmail.com>
2024-02-05 14:04:27 +0000

    slurm iris

---
+++ experiments/iris/pretrain.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --mem=1G
+#SBATCH -p shared
+#SBATCH --time=2-00:00:00
+#SBATCH --output=/nobackup/wclv88/iris/outs/slurm-%A_%a.out
+#SBATCH --array=0-1999
+
+module load python/3.10.8
+module load $PYTHON_BUILD_MODULES
+
+COUNT=10
+
+WANDB_DISABLED=true \
+stdbuf -oL \
+python3 \
+bounding-mi-repo/experiments/iris/benchmark.py \
+--seed 0 \
+--device cpu \
+--train_subject_models \
+--subject_model_count $COUNT \
+--subject_model_path /nobackup/wclv88/iris/standard/subject-models \
+--subject_model_num_classes 10 \
+--example_start_index $((SLURM_ARRAY_TASK_ID * COUNT))
